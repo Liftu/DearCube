298b8e92c1a95203d0b899f6d171956600dc7eb6
Liftu <louilou008@gmail.com>
2021-07-18 14:35:21 +0200

    Created the injectedThread based on GH's video

--- AssaultCube-Hack-Menu/dllmain.cpp
+++ AssaultCube-Hack-Menu/dllmain.cpp
@@ -1,6 +1,35 @@
 // dllmain.cpp : Définit le point d'entrée de l'application DLL.
 #include "pch.h"
 
+
+DWORD WINAPI injectedThread(HMODULE hModule)
+{
+    // Create consloe for debug purpose
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+
+    std::cout << "TOTO" << std::endl;
+
+    // Get module base
+    uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
+
+    // Hack loop
+    bool running = true;
+    while (running)
+    {
+        running = false;
+    }
+
+    // Key input
+
+    // Continue write/freeze
+
+    // Cleanup % eject
+
+    return 0;
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -8,11 +37,14 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 {
     switch (ul_reason_for_call)
     {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+        case DLL_PROCESS_ATTACH:
+        {
+            CloseHandle(CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)injectedThread, hModule, 0, NULL));
+        }
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
     }
     return TRUE;
 }
--- AssaultCube-Hack-Menu/framework.h
+++ AssaultCube-Hack-Menu/framework.h
@@ -3,3 +3,5 @@
 #define WIN32_LEAN_AND_MEAN             // Exclure les en-têtes Windows rarement utilisés
 // Fichiers d'en-tête Windows
 #include <windows.h>
+#include <TlHelp32.h>
+#include <iostream>
